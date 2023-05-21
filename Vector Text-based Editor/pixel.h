0f3f2baeb3dbc83a9f60592ceb5399d5e934322b
Josselin <josselin.keib@efrei.net>
2023-05-21 11:46:45 +0200

    Ajout pixel

---
+++ Vector Text-based Editor/pixel.h
@@ -0,0 +1,25 @@
+
+
+#ifndef VECTOR_TEXT_BASED_EDITOR_PIXEL_H
+#define VECTOR_TEXT_BASED_EDITOR_PIXEL_H
+
+
+#include "Shapes/Shape.h"
+
+struct pixel {
+    int px;
+    int py;
+};
+typedef struct pixel Pixel;
+
+Pixel* create_pixel(int px, int py);
+void delete_pixel(Pixel* pixel);
+void pixel_point(Shape* shape, Pixel*** pixel_tab, int* nb_pixels);
+void pixel_line(Shape* line, Pixel*** pixel_tab, int* nb_pixels);
+void pixel_circle(Shape* shape, Pixel*** pixel_tab, int*nb_pixels);
+void pixel_square(Shape* square, Pixel*** pixel_tab, int* nb_pixels);
+void pixel_rectangle(Shape* rectangle, Pixel*** pixel_tab, int* nb_pixels);
+void pixel_polygon(Shape* polygon, Pixel*** pixel_tab, int* nb_pixels);
+
+
+#endif //VECTOR_TEXT_BASED_EDITOR_PIXEL_H
